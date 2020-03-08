931dfc898539691e7223ef1dc912362ebcfbe060
Vitalii Savinov <vitsavinov@gmail.com>
2020-03-08 21:24:31 +0300

    Remove favicon

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const port = 80;
 
-var path         = require('path');
-var favicon      = require('serve-favicon');
+var path = require('path');
 
 const app = express();
-
-app.use(express.static(__dirname));
-app.use(favicon(path.join(__dirname,'favicon.png')));
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');    
