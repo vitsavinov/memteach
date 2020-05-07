9db507f0d558b8c4b8f45c5d7482bf0465524bf8
Vitalii Savinov <vitsavinov@gmail.com>
2020-05-07 21:23:39 +0300

    Functions for the authentification forms added

--- js/card-table.js
+++ js/card-table.js
@@ -5,101 +5,128 @@ import CardService from './services/card-service.js';
 import HttpService from './services/http-service.js';
 
 export default class CardTable extends Component {
-  
-  constructor({ element }) {
-    super(element);
-    this._element = element;
-    
-    // init layout
-    this._cardslist = [];
-    this._layout = {
-      "cell-1": {},"cell-2": {},"cell-3": {},"cell-4": {},"cell-5": {},"cell-6": {},
-      "cell-7": {},"cell-8": {},"cell-9": {},"cell-10": {},"cell-11": {},"cell-12": {},
-      "cell-13": {},"cell-14": {},"cell-15": {},"cell-16": {},"cell-17": {},"cell-18": {},
-      "cell-19": {},"cell-20": {},"cell-21": {},"cell-22": {},"cell-23": {},"cell-24": {},
-      "cell-25": {},"cell-26": {},"cell-27": {},"cell-28": {},"cell-29": {},"cell-30": {},
-      "cell-31": {},"cell-32": {},"cell-33": {},"cell-34": {},"cell-35": {},"cell-36": {}
-    };
-    this._cardservice = new CardService();
-    
-    // get the cards from the server
-    HttpService.get( `./data/cards.json` )
-    .then(
-        (cards) => { 
-          this._cardslist = cards; 
+    constructor({ element }) {
+        super(element);
+        this._element = element;
 
-          this._shuffle(this._layout, this._cardslist);
-          this._fillTable();
-        },
-        (error) => { console.log(error); }
-    );
+        // init layout
+        this._cardslist = [];
+        this._layout = {
+          "cell-1": {},"cell-2": {},"cell-3": {},"cell-4": {},"cell-5": {},"cell-6": {},
+          "cell-7": {},"cell-8": {},"cell-9": {},"cell-10": {},"cell-11": {},"cell-12": {},
+          "cell-13": {},"cell-14": {},"cell-15": {},"cell-16": {},"cell-17": {},"cell-18": {},
+          "cell-19": {},"cell-20": {},"cell-21": {},"cell-22": {},"cell-23": {},"cell-24": {},
+          "cell-25": {},"cell-26": {},"cell-27": {},"cell-28": {},"cell-29": {},"cell-30": {},
+          "cell-31": {},"cell-32": {},"cell-33": {},"cell-34": {},"cell-35": {},"cell-36": {}
+        };
+        this._cardservice = new CardService();
 
-  }
+        // get the cards from the server
+        HttpService.get( `./data/cards.json` )
+        .then(
+            (cards) => {
+              this._cardslist = cards;
 
-  //====================================================
-  // get random int from the range min - max
-  _getRandomInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  //====================================================
-  // shuffle the cards on the table
-  _shuffle(table, cards) {
-    let tmpTable = [];
-    let tmpCards = [];
-
-    // create an auxilary array of cells
-    for (let key in table) {
-      tmpTable.push(key);
+              this._shuffle(this._layout, this._cardslist);
+              this._fillTable();
+              this._bindAuthButtons();
+            },
+            (error) => { console.log(error); }
+        );
     }
 
-    // create an auxiliary array of cards
-    cards.forEach( (item) => {
-      tmpCards.push(item);
-    });
-    
-
-    // fill the layout with cards randomly
-    let curLen = tmpTable.length;
-    while (curLen) { 
-      let curCard = tmpCards.pop(); // take the first card and delete it from the list
-      let curIndex = 0;
-      let curKey = "";
-
-      // place this card in the random cell
-      curIndex = this._getRandomInRange(0, curLen-1);
-      curKey = tmpTable[curIndex];
-      table[curKey] = curCard;
-      tmpTable.splice(curIndex, 1); // delete this cell from the list
-      curLen = tmpTable.length;
-
-      // place the "twin" of this card in the another random cell
-      curIndex = this._getRandomInRange(0, curLen-1);
-      curKey = tmpTable[curIndex];
-      table[curKey] = curCard;
-      tmpTable.splice(curIndex, 1); // delete this cell from the list
-      curLen = tmpTable.length;
+    //====================================================
+    // get random int from the range min - max
+    _getRandomInRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-  }
 
-  //=====================================================
-  // fill the table with cards from the filled layout
-  _fillTable() {
-    let newImg = "";
-    let curTD = {};
+    //====================================================
+    // shuffle the cards on the table
+    _shuffle(table, cards) {
+        let tmpTable = [];
+        let tmpCards = [];
 
-	for (let key in this._layout) {
-      // draw the card
-      newImg = `<img class="card-face hide" src="${this._layout[key].imagePath}">`;
-      curTD = this._element.querySelector("#"+key);
+        // create an auxilary array of cells
+        for (let key in table) {
+            tmpTable.push(key);
+        }
 
-      curTD.innerHTML +=  newImg;
+        // create an auxiliary array of cards
+        cards.forEach( (item) => {
+            tmpCards.push(item);
+        });
 
-      // bind the click handler to the each card
-      this.on(curTD, 'click', (event) => {
-        this._cardservice.cardCheck(event.currentTarget, this._layout); // check the state of the table
-      });
+
+        // fill the layout with cards randomly
+        let curLen = tmpTable.length;
+        while (curLen) {
+              let curCard = tmpCards.pop(); // take the first card and delete it from the list
+              let curIndex = 0;
+              let curKey = "";
+
+              // place this card in the random cell
+              curIndex = this._getRandomInRange(0, curLen-1);
+              curKey = tmpTable[curIndex];
+              table[curKey] = curCard;
+              tmpTable.splice(curIndex, 1); // delete this cell from the list
+              curLen = tmpTable.length;
+
+              // place the "twin" of this card in the another random cell
+              curIndex = this._getRandomInRange(0, curLen-1);
+              curKey = tmpTable[curIndex];
+              table[curKey] = curCard;
+              tmpTable.splice(curIndex, 1); // delete this cell from the list
+              curLen = tmpTable.length;
+        }
     }
-  }
+
+    //=====================================================
+    // fill the table with cards from the filled layout
+    _fillTable() {
+        let newImg = "";
+        let curTD = {};
+
+        for (let key in this._layout) {
+            // draw the card
+            newImg = `<img class="card-face hide" src="${this._layout[key].imagePath}">`;
+            curTD = this._element.querySelector("#"+key);
+
+            curTD.innerHTML +=  newImg;
+
+            // bind the click handler to the each card on the table
+            this.on(curTD, 'click', (event) => {
+            this._cardservice.cardCheck(event.currentTarget, this._layout); // check the state of the table
+            });
+        }
+    }
+
+    //=====================================================
+    // bind the click handler to the register and login buttons
+    _bindAuthButtons() {
+        //  bind the click handler to the Login button
+        logBtn = this._element.querySelector("#btnLogin");
+        this.on(logBtn, 'click', (event) => {
+              this._cardservice.openLoginForm(event.currentTarget, this._element);
+         });
+
+        //  bind the click handler to the Register button
+        regBtn = this._element.querySelector("#btnRegister");
+        this.on(regBtn, 'click', (event) => {
+              this._cardservice.openRegisterForm(event.currentTarget, this._element);
+        });
+
+        //  bind the click handler to the Enter button in the Login form
+        entrLogBtn = this._element.querySelector("#btnEnterLogin");
+        this.on(entrLogBtn, 'click', (event) => {
+              this._cardservice.closeLoginForm(event.currentTarget, this._element);
+        });
+
+        //  bind the click handler to the Enter button in the Register form
+        entrRegBtn = this._element.querySelector("#btnEnterRegister");
+        this.on(entrRegBtn, 'click', (event) => {
+              this._cardservice.closeRegisterForm(event.currentTarget, this._element);
+        });
+    }
   
 }
--- js/services/card-service.js
+++ js/services/card-service.js
@@ -7,69 +7,101 @@ import Component from '../component.js';
 // Depending of results of checking it closes the cards or takes them off from the table.
 export default class CardService extends Component {
   
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this._cardsRegistry = [];
-	this.counter = 36;
-	this.clicks = 0;
-	
-	this.startFlag = false;
-	this.stopFlag  = false;
-	
-	this.stopwatch = document.querySelector('[data-component="counter"]');
-	this.stopwatch.innerText = "Clicks: " + this.clicks.toString();
-  }
+        this._cardsRegistry = [];
+        this.counter = 36;
+        this.clicks = 0;
 
-  //===================================================================
-  // check the current state of the table and make actions
-  cardCheck(curElement, curLayout) {
-    if ( !this.startFlag ) {
-	  	this.startFlag = true;
-	}
-	if ( curElement.innerHTML !== '<img src="./img/card-empty.jpg">' ) {   					  // check if this cell is empty already 
-		if ( !this.stopFlag ) {
-	  		this.clicks++;
-			this.stopwatch.innerText = "Clicks: " + this.clicks.toString();
-		}
+        this.startFlag = false;
+        this.stopFlag  = false;
 
-		// this cell is not empty - make actions
-	    if (this._cardsRegistry.length === 0) {   // if the registry is empty already 
-		  this._cardsRegistry.push(curElement); // push the card to the registry
-		  this.cardShow(curElement.lastChild); // open this card
-		} 
-		else if (this._cardsRegistry.length === 1) {      // if there is one opened card in the registry already
-		  if ( this._cardsRegistry[0] !== curElement ) {  // forbid two clicks on the one card
-			this._cardsRegistry.push(curElement);
-			this.cardShow(curElement.lastChild); // open card
+        this.stopwatch = document.querySelector('[data-component="counter"]');
+        this.stopwatch.innerText = "Clicks: " + this.clicks.toString();
+    }
 
-			// check two cards from the registry for coincidence
-			if ( curLayout[this._cardsRegistry[0].id].id === curLayout[this._cardsRegistry[1].id].id ) {
-			  let card_1 = this._cardsRegistry[0];
-			  let card_2 = this._cardsRegistry[1];
-			  this._cardsRegistry.length = 0;   // clean the registry
+    //===================================================================
+    // check the current state of the table and make actions
+    cardCheck(curElement, curLayout) {
+        if ( !this.startFlag ) {
+            this.startFlag = true;
+        }
+        if ( curElement.innerHTML !== '<img src="./img/card-empty.jpg">' ) {   					  // check if this cell is empty already
+            if ( !this.stopFlag ) {
+                this.clicks++;
+                this.stopwatch.innerText = "Clicks: " + this.clicks.toString();
+            }
 
-			  setTimeout( () => {
-				this.cardDrop(card_1); 			// remove these cards from the table
-				this.cardDrop(card_2);  		// 
-				
-				this.counter -= 2;
-				if ( this.counter === 0 ) {
-					this.stopFlag = true;
-				}
-			  }, 200);
-			}
-		  }
-		}
-		else if (this._cardsRegistry.length === 2) {    // if there are two non-equals opened cards in the registry already
-		  this.cardHide(this._cardsRegistry[0].lastChild); 
-		  this.cardHide(this._cardsRegistry[1].lastChild); // close these cards
-		  this._cardsRegistry.length = 0;
+            // this cell is not empty - make actions
+            if (this._cardsRegistry.length === 0) {   // if the registry is empty already
+              this._cardsRegistry.push(curElement); // push the card to the registry
+              this.cardShow(curElement.lastChild); // open this card
+            }
+            else if (this._cardsRegistry.length === 1) {      // if there is one opened card in the registry already
+              if ( this._cardsRegistry[0] !== curElement ) {  // forbid two clicks on the one card
+                this._cardsRegistry.push(curElement);
+                this.cardShow(curElement.lastChild); // open card
 
-		  this.cardShow(curElement.lastChild); // open new card
-		  this._cardsRegistry.push(curElement);     // push this card to the registry
-		}
-	}
+                // check two cards from the registry for coincidence
+                if ( curLayout[this._cardsRegistry[0].id].id === curLayout[this._cardsRegistry[1].id].id ) {
+                  let card_1 = this._cardsRegistry[0];
+                  let card_2 = this._cardsRegistry[1];
+                  this._cardsRegistry.length = 0;   // clean the registry
 
-  }            // end cardCheck()
+                  setTimeout( () => {
+                    this.cardDrop(card_1); 			// remove these cards from the table
+                    this.cardDrop(card_2);  		//
+
+                    this.counter -= 2;
+                    if ( this.counter === 0 ) {
+                        this.stopFlag = true;
+                    }
+                  }, 200);
+                }
+              }
+            }
+            else if (this._cardsRegistry.length === 2) {    // if there are two non-equals opened cards in the registry already
+              this.cardHide(this._cardsRegistry[0].lastChild);
+              this.cardHide(this._cardsRegistry[1].lastChild); // close these cards
+              this._cardsRegistry.length = 0;
+
+              this.cardShow(curElement.lastChild); // open new card
+              this._cardsRegistry.push(curElement);     // push this card to the registry
+            }
+        }
+    }            // end cardCheck()
+
+    //===================================================================
+    // handle click on Login button
+    openLoginForm(curTarget, curElement) {
+        curElement.querySelector('[data-component="counter"]').style.display='none';
+        curElement.querySelector('[data-component="auth-buttons"]').style.display='none';
+        curElement.querySelector('[data-component="login-form"]').style.display='inline-block';
+    }
+
+    //===================================================================
+    // handle click on Register button
+    openRegisterForm(curTarget, curElement) {
+        curElement.querySelector('[data-component="counter"]').style.display='none';
+        curElement.querySelector('[data-component="auth-buttons"]').style.display='none';
+        curElement.querySelector('[data-component="register-form"]').style.display='inline-block';
+    }
+
+    //===================================================================
+    // handle click on Enter button in the Login form
+    closeLoginForm(curTarget, curElement) {
+        curElement.querySelector('[data-component="login-form"]').style.display='none';
+        curElement.querySelector('[data-component="counter"]').style.display='inline-block';
+        curElement.querySelector('[data-component="auth-buttons"]').style.display='inline-block';
+    }
+
+    //===================================================================
+    // handle click on Enter button in the Register form
+    closeRegisterForm(curTarget, curElement) {
+        curElement.querySelector('[data-component="register-form"]').style.display='none';
+        curElement.querySelector('[data-component="counter"]').style.display='inline-block';
+        curElement.querySelector('[data-component="auth-buttons"]').style.display='inline-block';
+    }
+
 }
