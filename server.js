6a9501e3cd9f27c9807bf549158697b1c2221bec
Vitalii Savinov <vitsavinov@gmail.com>
2020-05-20 20:57:39 +0300

    Some license information added

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+//
+// Copyright (c) 2020, Vitalii Savinov, vitsavinov@gmail.com
+// Licensed under the ISC License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at https://www.isc.org/licenses/
+//
+
 const express = require('express');
 const port = 80;
 
